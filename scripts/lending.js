960628bf769537649ef9da2b5b672c45f47e910a
Kirill <kyrimer2010@gmail.com>
2024-02-25 21:36:29 +0300

    working links

---
+++ scripts/lending.js
@@ -0,0 +1,8 @@
+document.getElementById('login_button').onclick = myFunction;
+document.getElementsByClassName('button-with-no-back')[0].onclick = myFunction;
+document.getElementsByClassName('button-with-no-back')[1].onclick = myFunction;
+document.getElementsByClassName('button-with-no-back')[2].onclick = myFunction;
+
+function myFunction() {
+    window.location.href = "login.html";
+}
